5cac03f9af58062a9020197309bb0959effa34a1
Shawal Ahmad Mohmand <criticalmiind@gmail.com>
2022-04-03 00:42:42 +0500

    first commit

--- Basic/php-includes/connect.php
+++ Basic/php-includes/connect.php
@@ -1,14 +1,14 @@
 <?php
-	// $db_host = "localhost";
-	// $db_user = "eblpaki3";
-	// $db_pass = "Adilhassan008.";
-	// $db_name = "eblpaki3_basic";
+	$db_host = "localhost";
+	$db_user = "eblpaki3";
+	$db_pass = "Adilhassan008.";
+	$db_name = "eblpaki3_basic";
 
 	
-	$db_host = "localhost";
-	$db_user = "root";
-	$db_pass = "";
-	$db_name = "eblpaki3_basic";
+	// $db_host = "localhost";
+	// $db_user = "root";
+	// $db_pass = "";
+	// $db_name = "eblpaki3_basic";
 	
 	$con =  mysqli_connect($db_host,$db_user,$db_pass,$db_name);
 	if(mysqli_connect_error()){
