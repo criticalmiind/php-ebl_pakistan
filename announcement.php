68275fb4114b3e22a8be59c4700efed0a36b2af2
Shawal Ahmad Mohmand <criticalmiind@gmail.com>
2022-04-02 22:20:42 +0500

    first commit

--- announcement.php
+++ announcement.php
@@ -5,6 +5,11 @@
 ?>
     <div class="alert alert-warning" role="alert">
         <?php echo $announcement; ?>
+
+        </br></br>
+        <strong>خبردار:</strong>اسلام وعلیکم! کچھ  تیکنیکی وجوھات کی وجہ سے ہم سب ممبرز کو اگاہی دے رہے ہیں، کہ اکاونٹ نمبر (03450333262) پر کوئ پیسے جمہ نہ کرے، ورنہ بعد میں ہم ذمہ دار نہ ہوںگے، بہت جلد ہم نیا اکاونٹ نمبر دے دینگے! شکریہ!
+        </br></br></br>
+        Contact: info@eblpakistan.com
     </div>
 <?php } ?>
 <br/>
